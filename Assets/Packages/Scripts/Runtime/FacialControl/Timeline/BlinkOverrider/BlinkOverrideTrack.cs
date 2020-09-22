3544b8989428057c34bdb3b01a344b692b224000
murasaqi <uma9266@gmail.com>
2020-09-22 21:09:47 +0900

    ディレクトリ整理

--- Assets/Packages/Scripts/Runtime/FacialControl/Timeline/BlinkOverrider/BlinkOverrideTrack.cs
+++ Assets/Packages/Scripts/Runtime/FacialControl/Timeline/BlinkOverrider/BlinkOverrideTrack.cs
@@ -1,9 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Playables;
+﻿
 using UnityEngine.Timeline;
 using System.Linq;
+using UnityEngine;
+using UnityEngine.Playables;
 
 namespace CharacterAnimationEssentials.Facial
 {
